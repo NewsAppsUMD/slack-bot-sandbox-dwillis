42e7f45c1cec4d5ebcfca811565f90e712520a18
Derek Willis <dwillis@gmail.com>
2024-03-11 20:12:39 +0000

    congress slacking

--- congress.py
+++ congress.py
@@ -1,16 +1,22 @@
 import os
+import json
 import requests
 from slack import WebClient
 from slack.errors import SlackApiError
 
-url = "https://api.congress.gov/v3/committee-report/118?format=json"
-
-r = requests.get(url, headers={"x-api-key":congress_key}))
-
+congress_key = os.environ.get('CONGRESS_API_KEY')
 slack_token = os.environ.get('SLACK_API_TOKEN')
 
+url = f"https://api.congress.gov/v3/committee-report/118?format=json"
+
+r = requests.get(url, headers={'x-api-key': congress_key})
+
+reports = r.json()['reports']
+first_report = reports[0]
+
+msg = f"The latest committee report is {first_report['citation']}, which was issued on {first_report['updateDate']}. The url is {first_report['url']}"
+
 client = WebClient(token=slack_token)
-msg = "testing!"
 try:
     response = client.chat_postMessage(
         channel="slack-bots",
